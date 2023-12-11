ebb5eb68278114400bcff6fc8de180e702713fe3
Maria Racariu <90383391+MariaRacariu@users.noreply.github.com>
2023-12-11 17:17:57 +0100

    code

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+// Create a promise
+async function getAllCountriesList() {
+    const url = "https://restcountries.com/v3.1/all";
+    const response = await fetch(url);
+    console.log(response);
+
+    if(response.ok){
+        const data = await response.json();
+        console.log(data);
+        return data.message;
+    }else if(response.status === 404){
+        throw 404;
+    }
+}
+getAllCountriesList();
+
+// Get the form Element
+const countriesForm = document.querySelector('form');
+
+// Add event listener to form
+countriesForm.addEventListener('submit', event=>{
+    event.preventDefault();
+
+    // Get inputs and the users input value
+    const searchInput = document.querySelector('#searchInput').value;
+    const countryNameChoise = document.querySelector('#countryNameChoise').value;
+    const languageChoise = document.querySelector('#languageChoise').value;
+})
