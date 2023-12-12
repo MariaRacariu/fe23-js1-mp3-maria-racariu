ebedc0ad848983dc3f390e056545ceac3b3be09d
MariaRacariu <90383391+MariaRacariu@users.noreply.github.com>
2023-12-12 12:41:41 +0100

    tested code

--- javascript/main.js
+++ javascript/main.js
@@ -1,28 +1,87 @@
 // Create a promise
-async function getAllCountriesList() {
-    const url = "https://restcountries.com/v3.1/all";
-    const response = await fetch(url);
-    console.log(response);
+async function getAllCountriesList(path) {
+    const url = `https://restcountries.com/v3.1/${path}`;
 
-    if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        return data.message;
-    } else if (response.status === 404) {
-        throw 404;
+    try {
+        const response = await fetch(url);
+        // console.log(response);
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            showName(data[0].name.common);
+        } else if (response.status === 404) {
+            throw 404;
+        } else {
+            throw 'error';
+        }
+    }
+    catch (error) {
+        displayErrorMessage(error);
     }
 }
-getAllCountriesList();
+// getAllCountriesList("all");
 
 // Get the form Element
 const countriesForm = document.querySelector('form');
 
+const searchInput = document.querySelector('#searchInput').value;
+const countryNameChoice = document.querySelector('#countryNameChoice').value;
+const languageChoice = document.querySelector('#languageChoice').value;
+
+function handleInput(choice) {
+    console.log(choice);
+    if (choice.value === countryNameChoice) {
+        console.log('country name');
+        test(countryNameChoice);
+    } else if (choice.value === languageChoice) {
+        console.log('language');
+        test(languageChoice);
+    } else {
+        console.log('nothing');
+    }
+}
+
+
+
 // Add event listener to form
 countriesForm.addEventListener('submit', event => {
     event.preventDefault();
+    function test(choice) {
+        if (choice === countryNameChoice) {
+            findInfo(searchInput);
+        } else if (choice === languageChoice) {
 
-    // Get inputs and the users input value
-    const searchInput = document.querySelector('#searchInput').value;
-    const countryNameChoice = document.querySelector('#countryNameChoice').value;
-    const languageChoice = document.querySelector('#languageChoice').value;
+        } else {
+            displayErrorMessage();
+        }
+    }
+    test();
 })
+
+// Sends new request for new async promise
+function findInfo(countryName) {
+    getAllCountriesList(`name/${countryName}?fields=name`);
+}
+
+// Creates h1 and shows the country name
+function showName(countryName) {
+
+    const countryNameElement = document.createElement('h1');
+    document.body.append(countryNameElement);
+    countryNameElement.innerText = countryName;
+}
+
+// Display Error Message for user
+function displayErrorMessage(error) {
+    console.log(error);
+
+    const errorMessage = document.createElement('h1');
+
+    if (error === 404) {
+        errorMessage.innerText = '404';
+    } else {
+        errorMessage.innerText = 'something went wrong';
+    }
+    document.body.append(errorMessage);
+}
